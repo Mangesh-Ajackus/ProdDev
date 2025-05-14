75f65fcc0242f69d473f10fe97b3a7a12b73f480
Mangesh-Ajackus <105034425+Mangesh-Ajackus@users.noreply.github.com>
2025-05-14 16:08:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
               height={20}
             />
 
-            Deploy now - test - 3
+            Deploy now - Production
 
           </a>
           <a
